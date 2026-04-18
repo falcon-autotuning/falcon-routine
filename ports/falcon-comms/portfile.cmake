b1161213e861b9d9b6178e0f1a4b8bb65a457d4e
tjk144 <tkovach@wisc.edu>
2026-04-17 22:44:21 -0500

    Fixed the versions

--- ports/falcon-comms/portfile.cmake
+++ ports/falcon-comms/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/falcon-comms
     REF v${VERSION}
-    SHA512 4d60fd733fb967b25c85fe46e04719a9af3f84c77f3c5d7038802711dd912813b2c7d129b48bff3ec7b0d5bff71eb75e47fb51f31167b97602722c8ccc79af9f
+    SHA512 a31bd7a152cd5c39d58395654a3cfcac3fd24f608b0b3340f970884b4b9983d1ca0f87c8f3a4d502743f62188b5d97bc3d4b8fe6a6c764a4431c1d739fa8ba53
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
