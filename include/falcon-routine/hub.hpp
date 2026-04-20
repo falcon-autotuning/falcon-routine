6c249a6826fb54c0964eed0c2702a0f06d4acd75
tjk144 <tkovach@wisc.edu>
2026-04-20 14:27:50 -0500

    Added support for deserializing configs and ports

--- include/falcon-routine/hub.hpp
+++ include/falcon-routine/hub.hpp
@@ -1,17 +1,12 @@
 #pragma once
 #include "falcon-routine/export.h"
-#include <falcon-comms/routine_comms.hpp>
 #include <falcon-core/communications/messages/MeasurementRequest.hpp>
 #include <falcon-core/communications/messages/MeasurementResponse.hpp>
 #include <falcon-core/communications/messages/VoltageStatesResponse.hpp>
+#include <falcon-core/instrument_interfaces/names/Ports.hpp>
+#include <falcon-core/physics/config/core/Config.hpp>
 
 namespace falcon::routine {
-
-/**
- * @brief Alias for Routine Comms but exposed within the routine
- * library.
- */
-using Comms = falcon::comms::RoutineComms;
 
 /**
  * @brief Allows access to instrument hub to query current device state
@@ -33,4 +28,20 @@ request_measurement(
     const falcon_core::communications::messages::MeasurementRequestSP &req,
     int timeout_ms);
 
+/**
+ * @brief Request the device config from the instrument hub.
+ * @param timeout_ms the timeout in milliseconds to wait
+ * @return The Config if successful
+ */
+falcon_core::physics::config::core::ConfigSP FALCON_ROUTINE_API
+request_config(int timeout_ms);
+
+/**
+ * @brief Request the current port payload from the instrument hub.
+ * @param timeout_ms the timeout in milliseconds to wait
+ * @return The Ports if successful
+ */
+std::tuple<falcon_core::instrument_interfaces::names::Ports,
+           falcon_core::instrument_interfaces::names::Ports>
+    FALCON_ROUTINE_API request_port_payload(int timeout_ms);
 } // namespace falcon::routine
--- src/hub.cpp
+++ src/hub.cpp
@@ -1,4 +1,6 @@
 #include "falcon-routine/hub.hpp"
+#include <falcon-comms/routine_comms.hpp>
+#include <falcon-comms/runtime_comms.hpp>
 #include <falcon-core/communications/Time.hpp>
 #include <falcon-core/communications/messages/VoltageStatesResponse.hpp>
 #include <limits>
@@ -10,7 +12,7 @@ using falcon_core::communications::Time;
 // TODO: make subscribe_state_response take a long long
 falcon_core::communications::messages::VoltageStatesResponseSP
 request_device_state(int timeout_ms) {
-  Comms comms;
+  falcon::comms::RoutineComms comms;
   long long value = Time().time();
   if (value >= std::numeric_limits<int>::min() &&
       value <= std::numeric_limits<int>::max()) {
@@ -29,7 +31,7 @@ falcon_core::communications::messages::MeasurementResponseSP
 request_measurement(
     const falcon_core::communications::messages::MeasurementRequestSP &req,
     int timeout_ms) {
-  Comms comms;
+  falcon::comms::RoutineComms comms;
   std::string json_req = req->to_json_string();
   long long value = Time().time();
   if (value >= std::numeric_limits<int>::min() &&
@@ -42,6 +44,44 @@ request_measurement(
             falcon_core::communications::messages::MeasurementResponse>(
             outs[0]);
   }
-  throw std::runtime_error("Tiem is larger than an int");
+  throw std::runtime_error("Time is larger than an int");
+}
+
+// TODO: make subscribe_measure_response take a long long
+falcon_core::physics::config::core::ConfigSP FALCON_ROUTINE_API
+request_config(int timeout_ms) {
+  falcon::comms::RuntimeComms comms;
+  long long value = Time().time();
+  if (value >= std::numeric_limits<int>::min() &&
+      value <= std::numeric_limits<int>::max()) {
+    int result = static_cast<int>(value);
+    auto resp = comms.subscribe_config_response(timeout_ms, result);
+    return falcon_core::physics::config::core::Config::from_json_string<
+        falcon_core::physics::config::core::Config>(resp.response);
+  }
+  throw std::runtime_error("Time is larger than an int");
+}
+
+// TODO: make subscribe_measure_response take a long long
+std::tuple<falcon_core::instrument_interfaces::names::Ports,
+           falcon_core::instrument_interfaces::names::Ports>
+    FALCON_ROUTINE_API request_port_payload(int timeout_ms) {
+  falcon::comms::RuntimeComms comms;
+  long long value = Time().time();
+  if (value >= std::numeric_limits<int>::min() &&
+      value <= std::numeric_limits<int>::max()) {
+    int result = static_cast<int>(value);
+    auto resp = comms.subscribe_port_payload(timeout_ms, result);
+    auto knobs =
+        falcon_core::instrument_interfaces::names::Ports::from_json_string<
+            falcon_core::instrument_interfaces::names::Ports>(resp.knobs);
+    auto meters =
+        falcon_core::instrument_interfaces::names::Ports::from_json_string<
+            falcon_core::instrument_interfaces::names::Ports>(resp.meters);
+    return std::tuple<falcon_core::instrument_interfaces::names::Ports,
+                      falcon_core::instrument_interfaces::names::Ports>(knobs,
+                                                                        meters);
+  }
+  throw std::runtime_error("Time is larger than an int");
 }
 } // namespace falcon::routine
