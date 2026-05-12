5f1ef01a166fd743dd1ac51f66f1e4010e303ff8
tjk144 <tkovach@wisc.edu>
2026-05-12 13:49:04 -0500

    Updated the hub to include managing safe voltage changes

--- include/falcon-routine/hub.hpp
+++ include/falcon-routine/hub.hpp
@@ -65,6 +65,20 @@ communications::voltage_states::DeviceVoltageStatesSP FALCON_ROUTINE_API
 read_device_voltages(int timeout_ms);
 
 /**
+ * @brief Cache the config for quick access.
+ * @param config the config to cache
+ */
+void FALCON_ROUTINE_API cache_config(physics::config::core::ConfigSP config);
+
+/**
+ * @brief Reads the current config. Checks the cache first before
+ * requesting from the instrument hub.
+ * @param timeout_ms the timeout in milliseconds to wait if a request is needed
+ * @return The config if successful
+ */
+physics::config::core::ConfigSP FALCON_ROUTINE_API read_config(int timeout_ms);
+
+/**
  * @brief Gets the ohmics connected to voltage sources. Caches the result for
  * future calls.
  * @param timeout_ms the timeout in milliseconds to wait if a request is needed
@@ -74,42 +88,32 @@ read_device_voltages(int timeout_ms);
 physics::device_structures::ConnectionsSP FALCON_ROUTINE_API
 get_ohmics_connected_to_voltage_sources(int timeout_ms);
 
-// /**
-//  * @brief Gets/Computes the Gate Relations for the current device. Caches the
-//  * result for future calls.
-//  * @param timeout_ms the timeout in milliseconds to wait if a request is
-//  needed
-//  * @return The GateRelations for the current device.
-//  */
-// physics::device_structures::GateRelationsSP FALCON_ROUTINE_API
-// get_gate_relations(int timeout_ms);
-//
-// /**
-//  * @brief Finds the safe maximum and mimimum voltages for a set of gates.
-//  * @param connections the connections to check for safe voltages
-//  * @param bounds the voltage bounds to check for safe voltages
-//  * @param max_safe_difference the maximum safe difference between adjacent
-//  gates
-//  * @param timeout_ms the timeout in milliseconds to wait if a request is
-//  needed
-//  * @return A map of connections to their safe voltage bounds.
-//  */
-// generic::MapSP<physics::device_structures::Connections, math::Domain>
-//     FALCON_ROUTINE_API get_voltage_bounds(
-//         physics::device_structures::ConnectionsSP connections,
-//         math::DomainSP bounds, double max_safe_difference, int timeout_ms););
-//
-// /**
-//  * @brief Determines if the voltage change is safe.
-//  * @param proposed_voltages the proposed voltage changes to check
-//  * @param bounds the voltage bounds to check against
-//  * @param max_safe_difference the maximum safe difference between adjacent
-//  gates
-//  * @param timeout_ms the timeout in milliseconds to wait if a request is
-//  needed
-//  * @return bool indicating if the voltage change is safe or not.
-//  */
-// bool FALCON_ROUTINE_API safe_voltage_change(math::PointSP proposed_voltages,
-//                                             math::DomainSP bounds,
-//                                             double max_safe_difference,
-//                                             int timeout_ms);
+/**
+* @brief Gets/Computes the Gate Relations for the current device. Caches the
+* result for future calls.
+* @param timeout_ms the timeout in milliseconds to wait if a request is
+needed
+* @return The GateRelations for the current device.
+*/
+physics::device_structures::GateRelationsSP FALCON_ROUTINE_API
+get_gate_relations(int timeout_ms);
+
+/**
+* @brief Finds the safe maximum and mimimum voltages for a set of gates.
+* @param search_domain the domain to check for safe voltages
+* @param timeout_ms the timeout in milliseconds to wait if a request is
+needed
+* @return A map of connections to their safe voltage bounds.
+*/
+math::domains::CoupledLabelledDomainSP FALCON_ROUTINE_API get_voltage_bounds(
+    const instrument_interfaces::names::PortsSP search_domain, int timeout_ms);
+
+/**
+* @brief Determines if the voltage change is safe.
+* @param proposed_voltages the proposed voltage changes to check
+* @param timeout_ms the timeout in milliseconds to wait if a request is
+needed
+* @return bool indicating if the voltage change is safe or not.
+*/
+bool FALCON_ROUTINE_API safe_voltage_change(math::PointSP proposed_voltages,
+                                            int timeout_ms);
--- src/hub.cpp
+++ src/hub.cpp
@@ -83,6 +83,7 @@ request_port_payload(int timeout_ms) {
 const char *DEVICE_VOLTAGES_CACHE_NAME = "device_voltages";
 const char *OHMICS_CONNECTED_TO_VOLTAGE_SOURCES_CACHE_NAME =
     "ohmics_connected_to_voltage_sources";
+const char *CONFIG_CACHE_NAME = "config";
 void cache_device_voltages(
     falcon_core::communications::voltage_states::DeviceVoltageStatesSP
         voltages) {
@@ -102,6 +103,20 @@ read_device_voltages(int timeout_ms) {
   std::thread([voltages] { cache_device_voltages(voltages); }).detach();
   return voltages;
 }
+void cache_config(physics::config::core::ConfigSP config) {
+  return cache_item(CONFIG_CACHE_NAME, config->to_json_string());
+}
+physice::config::core::ConfigSP read_config(int timeout_ms) {
+  auto results = read_cache(CONFIG_CACHE_NAME);
+  if (!results.empty()) {
+    // Cache hit, return cached value
+    return falcon_core::physics::config::core::Config::from_json_string<
+        falcon_core::physics::config::core::Config>(results[0].characteristic);
+  }
+  auto config = request_config(timeout_ms);
+  std::thread([config] { cache_config(config); }).detach();
+  return config;
+}
 
 falcon_core::physics::device_structures::ConnectionsSP
 get_ohmics_connected_to_voltage_sources(int timeout_ms) {
@@ -112,7 +127,72 @@ get_ohmics_connected_to_voltage_sources(int timeout_ms) {
         from_json_string<falcon_core::physics::device_structures::Connections>(
             results[0].characteristic);
   }
-  // TODO: Fix this function when the falcon-core API is stabilized
-  return std::make_shared<physics::device_structures::Connections>();
+  auto payload = request_port_payload(timeout_ms);
+  auto knobs = std::get<0>(payload);
+  auto meters = std::get<1>(payload);
+
+  auto config = request_config(timeout_ms);
+  auto connections = config->ohmics();
+  physics::device_structures::ConnectionsSP
+      ohmics_connected_to_voltage_sources =
+          std::make_shared<physics::device_structures::Connections>();
+  auto raw_ports = *knobs->ports();
+  auto raw_ohmics = *connections.;
+  for (const auto &knob : raw_ports) {
+    auto knob_connection = knob->connection();
+    if (knob_connection->is_ohmic()) {
+      for (const auto &ohmic : raw_ohmics) {
+        if (*knob_connection == *ohmic) {
+          ohmics_connected_to_voltage_sources->push_back(ohmic);
+        }
+      }
+    }
+  }
+  // Remove duplicates to ensure uniqueness
+  std::sort(ohmics_connected_to_voltage_sources->begin(),
+            ohmics_connected_to_voltage_sources->end());
+  ohmics_connected_to_voltage_sources->erase(
+      std::unique(ohmics_connected_to_voltage_sources->begin(),
+                  ohmics_connected_to_voltage_sources->end()),
+      ohmics_connected_to_voltage_sources->end());
+  // Launch cache update asynchronously
+  std::thread([ohmics_connected_to_voltage_sources] {
+    cache_item(OHMICS_CONNECTED_TO_VOLTAGE_SOURCES_CACHE_NAME,
+               ohmics_connected_to_voltage_sources->to_json_string());
+  }).detach();
+  return ohmics_connected_to_voltage_sources;
+}
+physics::device_structures::GateRelationsSP get_gate_relations(int timeout_ms) {
+  auto config = request_config(timeout_ms);
+  return config->generate_gate_relations();
+}
+math::domains::CoupledLabelledDomainSP
+get_voltage_bounds(const instrument_interfaces::names::PortsSP search_domain,
+                   int timeout_ms) {
+  auto voltage_states = read_device_voltages(timeout_ms);
+  if (!voltage_states) {
+    throw std::runtime_error("Failed to read device voltages");
+  }
+  auto config = read_config(timeout_ms);
+  if (!config) {
+    throw std::runtime_error("Failed to read config");
+  }
+  VoltageConstraints constraints{
+      config->adjacency(), config->max_safe_diff(),
+      std::make_pair<double, double>(config->min_bound(), config->max_bound())};)
+
+  return constraints.compute_maximal_domain(search_domain, voltage_states);
+}
+bool FALCON_ROUTINE_API safe_voltage_change(math::PointSP proposed_voltages,
+                                            int timeout_ms) {
+  auto config = read_config(timeout_ms);
+  if (!config) {
+    throw std::runtime_error("Failed to read config");
+  }
+  VoltageConstraints constraints{
+      config->adjacency(), config->max_safe_diff(),
+      std::make_pair<double, double>(config->min_bound(), config->max_bound())};)
+
+  return constraints.validate_voltage_state(proposed_voltages);
 }
 } // namespace falcon::routine
